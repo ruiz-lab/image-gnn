7ff01910adf77096418df1903dc5acacee9c2049
caiodeberaldini <caio-soad@hotmail.com>
2024-04-12 11:09:18 -0400

    Metrics changes: add VAE loss to metrics

--- src/scripts/utils/metrics.py
+++ src/scripts/utils/metrics.py
@@ -2,33 +2,32 @@ import torch
 
 import numpy as np
 
+import torch.nn.functional as F
+
+from typing import Tuple
+
 
 def rmse(y, y_hat):
     N = y.shape[0]
     rmse = np.sqrt(np.sum((y - y_hat) ** 2) / N)
 
-    return np.mean(rmse)
+    return rmse
 
 def torch_rmse(y, y_hat):
     N = y.shape[0]
     rmse = torch.sqrt(torch.sum((y - y_hat) ** 2) / N)
 
-    return rmse.mean()
+    return rmse
 
-def ioa(y, y_hat):
-    ioa = 1 - (np.sum((y - y_hat) ** 2)) / (
-        np.sum((np.abs(y_hat - np.mean(y)) + np.abs(y - np.mean(y))) ** 2)
-    )
+def torch_kl_div(mu, logvar):
+    return -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
-    return ioa
+def torch_vae_loss(x, x_hat):
+    output, mu, logvar, _ = x_hat
 
-def torch_ioa(y, y_hat):
-    ioa = 1 - (torch.sum((y - y_hat) ** 2, dim=0)) / (
-        torch.sum(
-            (torch.abs(y_hat - torch.mean(y, dim=0)) + torch.abs(y - torch.mean(y, dim=0)))
-            ** 2,
-            dim=0,
-        )
-    )
+    reconstruction_loss = F.mse_loss(output, x, reduction="sum")
+    kl_loss = torch_kl_div(mu, logvar)
 
-    return ioa.mean()
+    beta = 0.5
+
+    return reconstruction_loss +  beta * kl_loss
