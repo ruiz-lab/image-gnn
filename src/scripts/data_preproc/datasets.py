8e3a815d4a1d0c07e5cf77a1296889247c557c66
Caio Deberaldini <caio-soad@hotmail.com>
2025-01-10 11:05:40 -0500

    Embedded dataset class code refactor

--- src/scripts/data_preproc/datasets.py
+++ src/scripts/data_preproc/datasets.py
@@ -1,5 +1,7 @@
 import sys
 
+import re
+
 import numpy as np
 
 import torch
@@ -23,7 +25,7 @@ from dataclasses import dataclass, field
 
 from typing import List, Dict
 
-_ = jnp.ones((1, 1))
+# _ = jnp.ones((1, 1))
 
 def build_datasets(ds_config):
     datasets  = []
@@ -57,159 +59,6 @@ class MNISTDataset(MNIST):
     def __len__(self):
         return super().__len__()
 
-class MNISTFullDataset(Dataset):
-    def __init__( self, root="data/", train=True, **kwargs):
-        super().__init__()
-
-        if train:
-            train_ds = MNIST(root, train=True, download=True)
-            test_ds = MNIST(root, train=False, download=True)
-            datasets = [train_ds, test_ds]
-        else:
-            test_ds = MNIST(root, train=False, download=True)
-            datasets = [test_ds]
-
-        self.transform = Compose([ToTensor()])
-
-        self._load_data(datasets)
-
-    def _load_data(self, datasets):
-        data = torch.empty((1, 28, 28), dtype=torch.uint8)
-        targets = torch.empty((1,), dtype=torch.uint8)
-        for ds in datasets:
-            data = torch.concat((data, ds.data), 0)
-            targets = torch.concat((targets, ds.targets))
-
-        self.data = data[1:, :]
-        self.targets = targets[1:]
-
-    def __getitem__(self, index):
-        img = self.data[index]
-
-        img = Image.fromarray(img.numpy(), mode="L")
-
-        if self.transform is not None:
-            img = self.transform(img)
-
-        data = Data()
-        data.x = torch.flatten(img, start_dim=1)
-        data.y = torch.flatten(img, start_dim=1)
-
-        return data
-
-    def __len__(self):
-        return len(self.data)
-
-class MNISTEmbeddedDataset(Dataset):
-    def __init__(
-        self, 
-        base_dir,
-        ker_width=10,
-        graph_connectivity="fully_connected",
-        **kwargs
-    ):
-        super().__init__()
-
-        self.base_dir = base_dir
-        self.kernel_width = ker_width
-        self.graph_connectivity = graph_connectivity
-        self.data = self._get_data()
-
-        @jax.jit
-        def compute_edge_weight_jax(data):
-            X_i = data[:, None, :]
-            X_j = data[None, :, :]
-
-            d_e = ((X_i - X_j) ** 2).sum(-1)
-
-            edge_weights = jnp.ravel(
-                jnp.exp(- d_e / (10 ** 2))
-            )[:, None]
-
-            return edge_weights
-
-        self.compute_edge_weight_jax = compute_edge_weight_jax
-
-    def _get_data(self):
-        return np.load(Path(self.base_dir))
-
-    def _build_graph(self, data, graph_connectivity, index, sample_size=100):
-        graph_data = Data()
-
-        pop_size = data.shape[0]
-        idx_prob = [1 / (pop_size - 1)] * pop_size
-        idx_prob[index] = 0
-        idx_sample = np.random.choice(pop_size, sample_size-1, replace=False, p=idx_prob)
-        idx_sample = np.append(idx_sample, index)
-
-        graph_data.data = torch.tensor(data[idx_sample, :-1], dtype=torch.float32)
-        graph_data.y = torch.tensor(data[idx_sample, -1], dtype=torch.int64)
-        graph_data.num_nodes = graph_data.data.shape[0]
-
-        X_i = graph_data.data[:, None, :]
-        X_j = graph_data.data[None, :, :]
-        d_e = ((X_i - X_j) ** 2).sum(-1)
-
-        graph_data.edge_weights = torch.flatten(
-            torch.exp(- d_e / (self.kernel_width ** 2))
-        )[:, None]
-
-        if graph_connectivity == "fully_connected":
-            graph_data.edge_index = torch.from_numpy(
-                np.array(
-                    list(
-                        product(
-                            range(graph_data.num_nodes), 
-                            range(graph_data.num_nodes)
-                        )
-                    )
-                ).T
-            ).to(torch.int64)
-
-        return graph_data
-
-    def _build_graph_jax(self, data, graph_connectivity, index, sample_size=100):
-        graph_data = Data()
-
-        pop_size = data.shape[0]
-        idx_prob = [1 / (pop_size - 1)] * pop_size
-        idx_prob[index] = 0
-        idx_sample = np.random.choice(pop_size, sample_size-1, replace=False, p=idx_prob)
-        idx_sample = np.append(idx_sample, index)
-
-        graph_data.data = torch.tensor(data[idx_sample, :-1], dtype=torch.float32)
-        graph_data.y = torch.tensor(data[idx_sample, -1], dtype=torch.int64)
-        graph_data.num_nodes = graph_data.data.shape[0]
-
-        edge_weights_jax = self.compute_edge_weight_jax(
-            jnp.asarray(data[idx_sample, :-1])
-        )
-        graph_data.edge_weights = torch.from_numpy(np.array(edge_weights_jax))
-
-        if graph_connectivity == "fully_connected":
-            graph_data.edge_index = torch.from_numpy(
-                np.array(
-                    list(
-                        product(
-                            range(graph_data.num_nodes), 
-                            range(graph_data.num_nodes)
-                        )
-                    )
-                ).T
-            ).to(torch.int64)
-
-        return graph_data
-
-
-    def __getitem__(self, index):
-        return self._build_graph_jax(
-            self.data, 
-            self.graph_connectivity, 
-            index
-        )
-
-    def __len__(self):
-        return len(self.data)
 
 class CIFAR10Dataset(CIFAR10):
     def __init__(
@@ -241,50 +90,8 @@ class CIFAR10Dataset(CIFAR10):
     def __len__(self):
         return super().__len__()
 
-class CIFAR10FullDataset(Dataset):
-    def __init__( self, root="data/", train=True, **kwargs):
-        super().__init__()
 
-        if train:
-            train_ds = CIFAR10(root, train=True, download=True)
-            test_ds = CIFAR10(root, train=False, download=True)
-            datasets = [train_ds, test_ds]
-        else:
-            test_ds = CIFAR10(root, train=False, download=True)
-            datasets = [test_ds]
-
-        self.transform = Compose([ToTensor()])
-
-        self._load_data(datasets)
-
-    def _load_data(self, datasets):
-        data = torch.empty((1, 28, 28), dtype=torch.uint8)
-        targets = torch.empty((1,), dtype=torch.uint8)
-        for ds in datasets:
-            data = torch.concat((data, ds.data), 0)
-            targets = torch.concat((targets, ds.targets))
-
-        self.data = data[1:, :]
-        self.targets = targets[1:]
-
-    def __getitem__(self, index):
-        img = self.data[index]
-
-        img = Image.fromarray(img.numpy(), mode="L")
-
-        if self.transform is not None:
-            img = self.transform(img)
-
-        data = Data()
-        data.x = torch.flatten(img, start_dim=1)
-        data.y = torch.flatten(img, start_dim=1)
-
-        return data
-
-    def __len__(self):
-        return len(self.data)
-
-class CIFAR10EmbeddedDataset(Dataset):
+class EmbeddedDataset(Dataset):
     def __init__(
         self, 
         base_dir,
@@ -295,6 +102,7 @@ class CIFAR10EmbeddedDataset(Dataset):
         super().__init__()
 
         self.base_dir = base_dir
+        self.ds_name = re.split(r"embeddings", base_dir.lower().split('/')[1])[0]
         self.kernel_width = ker_width
         self.graph_connectivity = graph_connectivity
         self.data = self._get_data()
@@ -342,7 +150,8 @@ class CIFAR10EmbeddedDataset(Dataset):
         )[:, None]
         graph_data.edge_weights[graph_data.edge_weights < 0.3] = 0.0
 
-        graph_data.data = (graph_data.data - self.mean) / self.std
+        graph_data.data = (graph_data.data - self.mean) / self.std \
+            if self.ds_name == 'cifar10' else graph_data.data
 
         if graph_connectivity == "fully_connected":
             graph_data.edge_index = torch.from_numpy(
@@ -377,7 +186,8 @@ class CIFAR10EmbeddedDataset(Dataset):
         graph_data.edge_weights = torch.from_numpy(np.array(edge_weights_jax))
         graph_data.edge_weights[graph_data.edge_weights < 0.3] = 0.0
 
-        graph_data.data = (graph_data.data - self.mean) / self.std
+        graph_data.data = (graph_data.data - self.mean) / self.std \
+            if self.ds_name == 'cifar10' else graph_data.data
 
         if graph_connectivity == "fully_connected":
             graph_data.edge_index = torch.from_numpy(
@@ -393,6 +203,8 @@ class CIFAR10EmbeddedDataset(Dataset):
 
         return graph_data
 
+    def _normalize(self, data):
+        return (data - self.mean) / self.std
 
     def __getitem__(self, index):
         return self._build_graph_jax(
